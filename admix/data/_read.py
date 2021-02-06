7201f5e6ba848f0916f93ee5b8a8cda3534d8069
Kangcheng Hou <kangchenghou@gmail.com>
2021-02-06 15:31:39 -0800

    remove _genotype import to fix tests

--- admix/data/_read.py
+++ admix/data/_read.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from pysnptools.snpreader import Bed
 import numpy as np
-from ._genotype import Genotype
 from ._utils import read_int_mat, write_int_mat
 
 def read_lanc(path: str) -> np.ndarray:
@@ -26,19 +25,7 @@ def read_haplo(path: str) -> np.ndarray:
     """
     return read_int_mat(path)
 
-    
-def read_plink(path: str) -> Genotype:
-    """Read plink file as a genotype object
 
-    Args:
-        path (str): the path to the plink file without suffix
-
-    Returns:
-        Genotype: the genotype project
-    """
-    data = Bed(path)
-    
-    return Genotype(X=None, indiv=None, snp=None)
     
     
 
