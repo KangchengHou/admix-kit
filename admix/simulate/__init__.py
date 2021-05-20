32e43e0fe4350beaec914fa2b335e8b643869b0e
Kangcheng Hou <kangchenghou@gmail.com>
2021-05-19 22:13:06 -0700

    remove case control import

--- admix/simulate/__init__.py
+++ admix/simulate/__init__.py
@@ -1,6 +1,6 @@
 from ._phenotype import (
     sample_case_control,
-    simulate_phenotype_case_control_1snp,
+    # simulate_phenotype_case_control_1snp,
     simulate_continuous_phenotype,
     simulate_continuous_phenotype_grm
 )
@@ -9,7 +9,7 @@ from ._genotype import simulate_hap
 
 __all__ = [
     "sample_case_control",
-    "simulate_phenotype_case_control_1snp",
+    # "simulate_phenotype_case_control_1snp",
     "simulate_continuous_phenotype",
     "simulate_continuous_phenotype_grm",
     "simulate_lanc",
