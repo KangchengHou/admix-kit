068f40ea820bab1d62ad3375d4a938801d8e5e66
Kangcheng Hou <kangchenghou@gmail.com>
2021-07-08 09:23:39 -0700

    add pca

--- admix/tools/_pca.py
+++ admix/tools/_pca.py
@@ -1,6 +1,37 @@
 import numpy as np
 import pandas as pd
-# import allel
+import dask.array as da
+import dask
+
+
+def pca(gn, n_components=10, n_power_iter=4, copy=True):
+    """
+    gn: (n_indiv, n_snp) matrix
+    """
+    # standardize to mean 0 and variance 1
+    # TODO: check inputs
+
+    n_indiv, n_snp = gn.shape
+    if copy:
+        gn = gn.copy()
+
+    mean_ = gn.mean(axis=0)
+    std_ = gn.std(axis=0)
+    gn -= mean_
+    gn /= std_
+
+    u, s, v = da.linalg.svd_compressed(gn, k=n_components, n_power_iter=n_power_iter)
+    u, s, v = dask.compute(u, s, v)
+
+    # calculate explained variance
+    exp_var = (s ** 2) / n_indiv
+    full_var = exp_var.sum()
+    exp_var_ratio = exp_var / full_var
+
+    coords = u[:, :n_components] * s[:n_components]
+
+    return coords
+
 
 # def pca(gn, n_components=10, copy=True):
 #     # standardize to mean 0 and variance 1
