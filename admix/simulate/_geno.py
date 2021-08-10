07cf67398346a4aa8150a448acf397a13b325eb4
Kangcheng Hou <kangchenghou@gmail.com>
2021-08-09 20:46:14 -0700

    add implementation based on Fst

--- admix/simulate/_geno.py
+++ admix/simulate/_geno.py
@@ -133,3 +133,95 @@ def admix_geno(
             "n_anc": n_anc,
         },
     )
+
+
+def admix_geno2(
+    n_indiv: int,
+    n_snp: int,
+    n_anc: int,
+    mosaic_size: float,
+    anc_props: np.ndarray = None,
+    ancestral_af_range: List[float] = [0.1, 0.9],
+    F_st: float = 0.3,
+) -> xr.Dataset:
+    """Simulate admixed genotype
+    The generative model is:
+        1. for each ancestry, the allele frequencies are drawn
+        2. for each individual, breakpoints are drawn from a Poisson process.
+            and the ancestry will be filled based a multinomial distribution with `n_anc`
+            components
+        3. for each SNP, given the ancestry and the allele frequencies, the haplotype
+            is drawn
+    Haplotype are simulated under some frequencies
+
+    Parameters
+    ----------
+    n_indiv : int
+        Number of individuals
+    n_snp : int
+        Number of SNPs
+    n_anc : int
+        Number of ancestries
+    mosaic_size : float
+        Expected mosaic size in # of SNPs
+    anc_props : list of float
+        Proportion of ancestral populations, if not specified, the proportion
+        is uniform over the ancestral populations.
+    ancestral_af_range: [float, float]
+        allele frequencies range.
+    F_st: float
+        Distance for the two populations that form the admixed population.
+
+    Returns
+    -------
+        xr.Dataset
+        Simulated admixed dataset
+
+    References
+    ----------
+    We follow Zaitlen et al. 2014, Nature Genetics
+    See description in subsection "Simulations with simulated genotypes" in Methods
+    """
+    assert n_anc == 2, "n_anc must be 2, only two-way admixture is currently supported"
+    # global ancestry
+    if anc_props is None:
+        anc_props = np.ones(n_anc) / n_anc
+    else:
+        anc_props = np.array(anc_props)
+        assert np.sum(anc_props) == 1, "anc_props must sum to 1"
+    anc_props = np.array(anc_props)
+    assert anc_props.size == n_anc, "anc_props must have the same length as n_anc"
+
+    # local ancestry
+    rls_lanc = _lanc(n_indiv * 2, n_snp, mosaic_size=mosaic_size, anc_props=anc_props)
+    # n_indiv x n_snp x 2 (2 for each haplotype)
+    rls_lanc = np.dstack([rls_lanc[0:n_indiv, :], rls_lanc[n_indiv:, :]])
+    rls_geno = np.zeros_like(rls_lanc)
+
+    ancestral_afs = np.random.uniform(
+        low=ancestral_af_range[0], high=ancestral_af_range[1], size=n_snp
+    )
+
+    # allele frequencies for the two populations
+    allele_freqs = [
+        np.random.beta(
+            a=ancestral_afs * (1 - F_st) / F_st,
+            b=(1 - ancestral_afs) * (1 - F_st) / F_st,
+        )
+        for _ in range(n_anc)
+    ]
+
+    for i_anc in range(n_anc):
+        rls_geno[rls_lanc == i_anc] = np.random.binomial(
+            n=1, p=allele_freqs[i_anc][np.where(rls_lanc == i_anc)[1]]
+        )
+
+    return xr.Dataset(
+        data_vars={
+            "geno": (["indiv", "snp", "ploidy"], da.from_array(rls_geno)),
+            "lanc": (["indiv", "snp", "ploidy"], da.from_array(rls_lanc)),
+        },
+        attrs={
+            "n_anc": n_anc,
+        },
+    )
