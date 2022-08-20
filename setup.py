be78a65c83b576623fdb3b033ac9803c0f60a4c5
Kangcheng Hou <kangchenghou@gmail.com>
2022-08-20 15:23:28 -0700

    update versioning

--- setup.py
+++ setup.py
@@ -2,18 +2,38 @@ from distutils.version import LooseVersion
 import setuptools
 from setuptools import setup
 
-# Added support for environment markers in install_requires.
-if LooseVersion(setuptools.__version__) < "36.2":
-    raise ImportError("setuptools>=36.2 is required")
+exec(open("admix/version.py").read())
 
 setup(
     name="admix-kit",
-    version="0.1",
+    version=__version__,
     description="Tool kits for analyzing genetics data of admixed population",
     url="https://github.com/kangchenghou/admix-kit",
     author="Kangcheng Hou",
     author_email="kangchenghou@gmail.com",
     packages=["admix"],
     setup_requires=["numpy>=1.10"],
+    python_requires=">=3.7",
+    install_requires=[
+        "Cython",
+        "numpy",
+        "pandas",
+        "scipy",
+        "matplotlib",
+        "dask[array]>=2021.11.2",
+        "tqdm",
+        "xarray",
+        "statsmodels",
+        "structlog",
+        "fire",
+        "pylampld @ git+https://github.com/bogdanlab/lamp-ld.git",
+        "tinygwas @ git+https://github.com/bogdanlab/tinygwas.git",
+        "dask-pgen @ git+https://github.com/KangchengHou/dask-pgen.git",
+    ],
     entry_points={"console_scripts": ["admix=admix.cli:cli"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Intended Audience :: Science/Research",
+    ],
 )
