ad0b7b216bb3889f5aa466c664477715c56c7b22
Kangcheng Hou <kangchenghou@gmail.com>
2022-08-21 11:08:47 -0700

    update module import

--- admix/__init__.py
+++ admix/__init__.py
@@ -1,6 +1,6 @@
 from ._logging import logger
-from .dataset import Dataset
-from . import data, dataset, simulate, ancestry, plot, assoc, estimate, tools, io, cli
+from .dataset._dataset import Dataset
+from . import data, simulate, ancestry, plot, assoc, estimate, tools, io, cli
 
 __all__ = [
     "data",
