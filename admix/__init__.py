800b795c25edf1e18c6d83d34b3e4a1a4ea1b742
Kangcheng Hou <kangchenghou@gmail.com>
2022-08-21 11:14:47 -0700

    import dataset first

--- admix/__init__.py
+++ admix/__init__.py
@@ -1,5 +1,6 @@
 from ._logging import logger
-from .dataset._dataset import Dataset
+from . import dataset
+from .dataset import Dataset
 from . import data, simulate, ancestry, plot, assoc, estimate, tools, io, cli
 
 __all__ = [
