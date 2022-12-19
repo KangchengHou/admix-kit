607d0eac04530628343a9f0cb091a85fe13ae1ae
Kangcheng Hou <kangchenghou@gmail.com>
2022-12-18 21:16:14 -0800

    include versioning

--- admix/__init__.py
+++ admix/__init__.py
@@ -1,6 +1,7 @@
 from ._logging import logger
 from .dataset import Dataset
 from . import data, dataset, simulate, ancestry, plot, assoc, estimate, tools, io, cli
+from .version import __version__
 
 __all__ = [
     "data",
